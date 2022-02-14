7654d41998af3449afb978873cae6dedfebe975d
amenga666 <panmeng_a@163.com>
2022-02-15 00:22:49 +0800

    新增对话框

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div style="padding: 10px">
+<!--    新增、查询、导入、导出-->
     <div>
-      <el-button type="primary">新增</el-button>
+      <el-button type="primary" @click="add">新增</el-button>
       <el-button type="primary">导入</el-button>
       <el-button type="primary">导出</el-button>
       <el-input v-model="search" placeholder="请输入关键字" style="padding: 0 12px;width: 20%"/>
       <el-button type="primary">查询</el-button>
     </div>
+<!--    表格-->
     <el-table :data="tableData" style="width: 100%">
       <el-table-column fixed prop="date" label="Date"/>
       <el-table-column prop="name" label="Name"/>
@@ -15,6 +17,7 @@
       <el-table-column prop="address" label="Address"/>
       <el-table-column prop="zip" label="Zip"/>
       <el-table-column fixed="right" label="Operations">
+<!--        编辑、删除-->
         <template #default>
           <el-button type="primary" size="small" @click="handleEdit">编辑</el-button>
           <el-popconfirm title="确认删除？">
@@ -25,6 +28,7 @@
         </template>
       </el-table-column>
     </el-table>
+<!--    分页-->
     <el-pagination
         style="margin: 10px"
         v-model:currentPage="currentPage"
@@ -36,10 +40,31 @@
         @current-change="handleCurrentChange"
     >
     </el-pagination>
+<!--    新增对话框-->
+    <el-dialog
+        v-model="dialogVisible"
+        title="Tips"
+        width="30%"
+    >
+<!--      表单-->
+      <el-form ref="form" :model="form" label-width="120px">
+        <el-form-item label="用户名">
+          <el-input v-model="form.username" style="width: 66%"></el-input>
+        </el-form-item>
+      </el-form>
+<!--      对话框按钮-->
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">确定</el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+
 
 export default {
   name: 'Home',
@@ -50,6 +75,8 @@ export default {
       currentPage: '',
       pageSize: '',
       total: 10,
+      dialogVisible: false,
+      form: {},
       tableData: [
         {
           date: '2016-05-03',
@@ -64,6 +91,10 @@ export default {
     }
   },
   methods: {
+    add() {
+      this.dialogVisible = true
+      this.form = {}
+    },
     handleEdit() {
 
     },
@@ -75,7 +106,7 @@ export default {
     },
     handleCurrentChange() {
 
-    }
+    },
   }
 }
 </script>
